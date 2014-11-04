8b581edcfbdaac45738a356fdfe3174196d88076
Dennis Caldwell <dennis.caldwell@gmail.com>
2014-11-03 20:20:39 -0500

    Root page is limping along.

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,10 +1,17 @@
+import com.bipedalprogrammer.bookmarks.services.BookmarkServer
+import com.bipedalprogrammer.bookmarks.model.Bookmark
+
 import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
+
+BookmarkServer bookmarkServer = new BookmarkServer()
+
+bookmarkServer.addBookmark(new Bookmark('http://localhost:5050', 'This Page', ['Sample'], 'Something to get us started.'))
 
 ratpack {
   handlers {
     get {
-      render groovyTemplate("index.html", title: "Bookmarks")
+      render groovyTemplate("index.html", title: "Bookmarks", bookmarks: bookmarkServer.list())
     }
         
     assets "public"

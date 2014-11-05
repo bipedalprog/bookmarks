4fad92afd6390a8494648633924eb9fffc57ba7b
Dennis Caldwell <dennis.caldwell@gmail.com>
2014-11-05 15:54:22 -0500

    Reformatted to use two-spaced tabs. Missed that before.

--- src/main/groovy/com/bipedalprogrammer/bookmarks/model/Bookmark.groovy
+++ src/main/groovy/com/bipedalprogrammer/bookmarks/model/Bookmark.groovy
@@ -6,39 +6,39 @@ import java.time.LocalDateTime
  * Created by dennis on 10/16/14.
  */
 class Bookmark {
-    LocalDateTime created
-    LocalDateTime updated
-    String title
-    String url
-    List tags
-    String notes
+  LocalDateTime created
+  LocalDateTime updated
+  String title
+  String url
+  List tags
+  String notes
 
-    Bookmark(String url, String title, List<String> tags, String notes) {
-        this.url = url
-        this.title = title
-        this.tags = tags
-        this.notes = notes
-        this.created = LocalDateTime.now()
-        this.updated = LocalDateTime.now()
-    }
+  Bookmark(String url, String title, List<String> tags, String notes) {
+    this.url = url
+    this.title = title
+    this.tags = tags
+    this.notes = notes
+    this.created = LocalDateTime.now()
+    this.updated = LocalDateTime.now()
+  }
 
-    String toString() {
-        "title: $title url: $url tags: $tags notes: $notes"
-    }
+  String toString() {
+    "title: $title url: $url tags: $tags notes: $notes"
+  }
 
-    void setTitle(String title) {
-        this.title = title
-        this.updated = LocalDateTime.now()
-    }
+  void setTitle(String title) {
+    this.title = title
+    this.updated = LocalDateTime.now()
+  }
 
-    void setTags(List tags) {
-        this.tags = tags
-        this.updated = LocalDateTime.now()
-    }
+  void setTags(List tags) {
+    this.tags = tags
+    this.updated = LocalDateTime.now()
+  }
 
-    void setNotes(String notes) {
-        this.notes = notes
-        this.updated = LocalDateTime.now()
-    }
+  void setNotes(String notes) {
+    this.notes = notes
+    this.updated = LocalDateTime.now()
+  }
 
 }

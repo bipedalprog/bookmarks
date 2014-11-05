7713637f23b9e1899433c9d11bcc4b2edf65d9cb
Dennis Caldwell <dennis.caldwell@gmail.com>
2014-11-04 21:55:17 -0500

    Looks awful, but it works.

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,5 +1,6 @@
 import com.bipedalprogrammer.bookmarks.services.BookmarkServer
 import com.bipedalprogrammer.bookmarks.model.Bookmark
+import ratpack.form.Form
 
 import static ratpack.groovy.Groovy.groovyTemplate
 import static ratpack.groovy.Groovy.ratpack
@@ -13,7 +14,22 @@ ratpack {
     get {
       render groovyTemplate("index.html", title: "Bookmarks", bookmarks: bookmarkServer.list())
     }
-        
+
+    post('bookmarks') {
+      Form form = parse(Form)
+      if (form.title && form.url) {
+        List tags = []
+        if (form.tags) {
+          String tagString = form.tags
+          String[] stringArray = tagString.split(',')
+          stringArray.each({tags << it.trim()})
+        }
+        bookmarkServer.addBookmark(new Bookmark(form.url, form.title, tags, form.note))
+        println "We now have ${bookmarkServer.list().size()} bookmarks."
+        redirect('/')
+      }
+    }
+
     assets "public"
   }
 }
